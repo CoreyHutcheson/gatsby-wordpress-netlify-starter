9e2cdd11ec593202d3e95c7238b1175adf8a3e2d
Corey <hutcheson.coreyJ@gmail.com>
2019-04-11 16:41:22 -0500

    Initial setup of starter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+/**
+ * Import global scss files here
+ */
+import "css-reset-and-normalize/scss/reset-and-normalize.scss";
+import "src/utils/styles/global.scss";
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Wordpress Netlify Starter`,
+    description: `Get setup quickly running a wordpress back-end that get converted to a static site by gatsby and hosted on netlify with this starter theme.`,
+    author: `Corey Hutcheson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-root-import`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // Remove protocol and trailing slash
+        baseUrl: `demo.wp-api.org`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        // Set to false if not using Advanced Custom Fields wordpress plugin
+        useACF: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-wordpress-netlify-starter`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
